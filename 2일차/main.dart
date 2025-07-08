5000e0bdd4f8e4cbef0f0577758a68dc59bf3d4d
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-08 12:59:34 +0900

    quiz.1 switch 문을 if else 문으로 변경해보자

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  String a = 'banana';
+
+  // TODO: switch 문을 if else 문으로 변경해보자
+  switch (a) {
+    case '1':
+      print("object");
+      break;
+    case 'apple':
+      print("apple!");
+      break;
+    default:
+      print("no cases");
+      break;
+  }
+}
