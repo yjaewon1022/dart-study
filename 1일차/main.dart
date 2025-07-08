819109524d7fefe3828024a9aeede87b28f230c1
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-08 11:09:44 +0900

    두번째 커밋

--- 1일차/main.dart
+++ 1일차/main.dart
@@ -1,13 +1,19 @@
+// https://github.com/drsungwon/Dart-flutter-book
+
 void main() {
   // 정수형 변수 선언
   int num1 = 4;
+  print(num1);
 
   // 실수형
   double num2 = 1.1234;
+  print(num2);
 
   // 값을 변경할 수 없는 변수
   const num3 = 123;
   final num4 = 456;
+  print(num3);
+  print(num4);
 
   // 값을 유동적으로 변경할 수 있는 자료형
   dynamic variable = 13;
