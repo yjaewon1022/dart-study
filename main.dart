ed8429260c6e4cbff5c3197c50e2e425bf0dc1c6
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-08 10:27:34 +0900

    첫번째 커밋

---
+++ main.dart
@@ -0,0 +1,34 @@
+void main() {
+  // 정수형 변수 선언
+  int num1 = 4;
+
+  // 실수형
+  double num2 = 1.1234;
+
+  // 값을 변경할 수 없는 변수
+  const num3 = 123;
+  final num4 = 456;
+
+  // 값을 유동적으로 변경할 수 있는 자료형
+  dynamic variable = 13;
+  variable = '123';
+
+  // 연산자
+  // + - * / ~/ % -- ++ += -=
+
+  // 문자 출력
+  print("number $variable");
+
+  // 들여쓰기
+  print('123\n456');
+
+  String char = """
+sdfgsdfg
+
+
+
+asdfasdfasd
+""";
+
+  print(char);
+}
