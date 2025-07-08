781a00d4f203b1f22b90420754d7a366d3927793
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-08 15:36:31 +0900

    함수 named parameter 학습

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,25 +1,16 @@
-void main() {
-  // TODO: 구구단 1~9단 중 짝수단만 출력하는 반복문 작성 2,4,6,8단
-  // // 1. for 문 사용
-  // int dan, number;
-  // for (dan = 2; dan <= 8; dan = dan + 2) {
-  //   for (number = 1; number < 10; number++) {
-  //     print("$dan x $number = ${dan * number}");
-  //   }
-  // }
-
-  // 2. while 문 사용
-  int dan = 2, number = 1;
-
-  while (dan <= 8) {
-    while (number <= 9) {
-      print("$dan x $number = ${dan * number}");
-
-      number++;
-    }
-
-    number = 1;
-
-    dan = dan + 2;
+// 반환 값이 있는 함수
+int getMax(int num1, int num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
   }
+}
+
+void createPlayer({required String name, int level = 1, double exp = 0.00}) {
+  print('Player $name is $level Level and $exp exp.');
+}
+
+void main() {
+  createPlayer(name: 'jaewon', level: 5, exp: 0.00);
 }
