fe6dbd265c4c268ab1949367f8f8f8656dec5fa0
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-09 09:54:33 +0900

    day2 1번 퀴즈 정답

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -3,6 +3,27 @@
 // 91~100: A, 81~90: B, 71~80: C, 61~70: D, 51~60: E, 그 외: F
 // [철수의 시험 점수는 $score 점, 이며, 등급은 $grade 입니다.]
 
+void main() {
+  int score = 70;
+  String grade = '';
+
+  if (score >= 91 && score <= 100) {
+    grade = 'A';
+  } else if (score >= 81 && score <= 90) {
+    grade = "B";
+  } else if (score >= 71 && score <= 80) {
+    grade = "C";
+  } else if (score >= 61 && score <= 70) {
+    grade = "D";
+  } else if (score >= 51 && score <= 60) {
+    grade = "E";
+  } else {
+    grade = 'F';
+  }
+
+  print('철수의 시험 점수는 $score 점, 이며, 등급은 $grade 입니다.');
+}
+
 // 2. 키(176) 과 몸무게(63) 을 가진 영재는 본인의 BMI 가 궁금해졌습니다.
 //    본인의 BMI 를 구하는 함수를 작성한 후, 영재의 BMI의 점수와, 점수에 따른 기준표 결과를 출력하는 코드를 작성해보세요.
 //  ~ 18.4: 저체중 18.5 ~ 22.9: 정상, 23.0 ~ 24.9: 과체중, 25.0 ~ : 비만
