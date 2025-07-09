28846b5f03952f437bc60705f7a4312efb0953cd
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-09 15:47:37 +0900

    class 함수 만들기 기초1 코드

--- 3일차/main.dart
+++ 3일차/main.dart
@@ -1,37 +1,70 @@
-void main() {
-  // TODO: 문자열 "가" ~ "사" 를 포함한 Set 를 하나 생성하고,
-  //       "라" ~ "하" 를 포함한 Set 를 생성한 뒤,
-  //       두 세트를 합친 새로운 세트를 변수에 담아 출력해보자.
-  Set set1 = {"가", "나", "다", "라", "마", "바", "사"};
-  Set set2 = {"라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"};
-  // set1.addAll(set2);
-  // print(set1);
+class Integer {
+  late int _value;
 
-  // TODO: Set 심화과정 - 두 개의 세트에서 교집합, 합집합, 차집합을 새로운 세트들로 출력해보자
-  Set set4 = set1.intersection(set2); // 교집합
-  print(set4);
-  Set set5 = set1.difference(set2); // 차집합
-  print(set5);
-  Set set6 = set1.union(set2); // 합집합
-  print(set6);
-
-  // TODO: 과일 상점을 뜻하는 shop 변수에
-  //       apple 은 10개, banana 는 5개, mango 는 3개가 존재하도록 Map 을 생성한 뒤,
-  //       이 상점에서 망고를 판매하고 있는지, 판매하고 있다면
-  //       [이 상점에서는 망고를 $count 개 판매하고 있습니다.]
-  //       라고 출력하도록 해보자.
-  Map shop = {"apple": 10, "banana": 5, "mango": 3};
-  int count = 0;
-
-  if (shop.containsKey('mango')) {
-    count = shop["mango"];
-
-    print("이 상점에서는 망고를 $count 개 판매하고 있습니다.");
+  // 생성자
+  Integer([int givenValue = 0]) {
+    _value = givenValue;
   }
 
-  // TODO: Map 심화과정 - 상점에서 망고를 2개 판매했다고 가정한 후,
-  //       과일 상점에서 현재 팔고있는 망고의 총 개수를 출력해보자
-  shop['mango'] = shop['mango'] - 2;
+  int get() {
+    return _value;
+  }
 
-  print(shop);
+  void set(int getValue) {
+    _value = getValue;
+  }
+
+  String get asString => "$_value";
+
+  // TODO: int 형태로 반환하는 한 줄 getter 작성
+  int get asInt => _value;
+
+  set value(int givenInteger) => _value = givenInteger;
+}
+
+class TimemachineInteger extends Integer {}
+
+class StringClass {
+  late String _value;
+
+  // 생성자
+  StringClass([String givenString = '']) {
+    _value = givenString;
+  }
+
+  String get() {
+    return _value;
+  }
+
+  void set(String givenString) {
+    _value = givenString;
+  }
+}
+
+class Player {
+  // TODO: 인스턴스 변수로 name, age 를 선언
+
+  // TODO: 생성자 작성
+
+  // TODO: 사용자의 이름을 반환하는 getter 함수
+  // TODO: 사용자의 나이를 반환하는 getter 함수
+
+  // TODO: 사용자의 이름을 수정하는 setter 함수
+  // TODO: 사용자의 나이를 수정하는 setter 함수
+}
+
+void main() {
+  var int1 = Integer();
+  var int2 = Integer(3);
+
+  // var string1 = StringClass('안녕?');
+
+  // print(string1.get());
+  // string1.set('잘가');
+  // print(string1.get());
+
+  // int1.set(10);
+  int1.value = 112430;
+
+  print(int1.asInt);
 }
