5bdfad4397caf8c739caea69c0bef2f42d32b1ce
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 17:09:29 +0900

    Update quiz1.dart

--- 5일차/quiz1.dart
+++ 5일차/quiz1.dart
@@ -1,7 +1,7 @@
 // 상점을 뜻하는 Shop 제네릭 클래스를 구현하고, 비어있는 부분을 작성해 코드를 완성하세요.
 
 void main() {
-  var stringShop = ________; // 정수만 다룰 수 있어야 함.
+  var stringShop = ________; // 문자열만 다룰 수 있어야 함.
   var doubleShop = __________; // 실수만 다룰 수 있어야 함.
 
   print(stringShop.get()); // [나시] 를 출력해야 함.
