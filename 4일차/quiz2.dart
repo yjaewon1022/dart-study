7cf64d6d3f9ef532f73d92b9cccc7d9c2b2c77d9
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 10:09:12 +0900

    4일차 퀴즈 풀이

--- 4일차/quiz2.dart
+++ 4일차/quiz2.dart
@@ -4,3 +4,43 @@
 // 생성자를 작성할 땐 named parameter 를 이용해야 합니다.
 // 이후 사용자의 정보를 출력하는 함수인 profile 을 구현해야 하며, 다음의 내용을 출력해야 합니다.
 // [안녕하세요. 제 이름은 $name 이며, $age 살 $gender 입니다.]
+class Human {
+  late String name;
+  late int age;
+  late String gender;
+
+  // // 기본적으로 생성자를 만드는 방법
+  // Human(String givenName, int givenAge, String givenGender)
+  //   : age = givenAge,
+  //     name = givenName,
+  //     gender = givenGender;
+
+  // named parameter를 이용해 생성자를 만드는 방법
+  Human({
+    required String givenName,
+    required int givenAge,
+    required String givenGender,
+  }) {
+    age = givenAge;
+    name = givenName;
+    gender = givenGender;
+  }
+
+  // void profile() {
+  //   print("안녕하세요. 제 이름은 $name 이며, $age 살 $gender 입니다.");
+  // }
+
+  String profile() {
+    return "안녕하세요. 제 이름은 $name 이며, $age 살 $gender 입니다.";
+  }
+}
+
+void main() {
+  // named paramter 를 사용하지 않았을 때 클래스를 호출하는 방법
+  // var asdf = Human("짱구", 5, "남성");
+
+  // named paramter 를 사용했을 때 클래스를 호출하는 방법
+  var asdf = Human(givenGender: "남성", givenName: "철수", givenAge: 5);
+
+  print(asdf.profile());
+}
--- 4일차/quiz3.dart
+++ 4일차/quiz3.dart
@@ -2,3 +2,29 @@
 // 이후 Boat 와 Car 라는 클래스를 구현한 후,
 // 각각의 클래스 별 drive 함수 실행시 다음의 내용을 출력하도록 구현하세요.
 // Boat : [보트가 물 위를 떠다닙니다.], Car : [자동차가 도로를 달려다닙니다.]
+
+abstract class Vehicle {
+  void drive();
+}
+
+class Boat implements Vehicle {
+  @override
+  drive() {
+    print("보트가 물 위를 떠다닙니다.");
+  }
+}
+
+class Car implements Vehicle {
+  @override
+  drive() {
+    print("자동차가 도로를 달려다닙니다");
+  }
+}
+
+void main() {
+  var car = Car();
+  var boat = Boat();
+
+  car.drive();
+  boat.drive();
+}
