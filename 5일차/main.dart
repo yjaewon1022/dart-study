fca3ad74bc06584b1cb29a06769280049742b323
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 16:20:11 +0900

    try on 학습

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -7,13 +7,13 @@ void main() {
   // catch = try 중 에러가 발생하면, try 실행을 멈추고 catch 이후의 내용을 실행해라.
 
   // try-catch 문법 사용 방법: try {} catch(error) {}
+  // try-on 문법 사용 방법: try {} on 에러이름 {}
   try {
     result = num1 ~/ num2; // 1 ~/ 0
-  } catch (error) {
-    print("catch: $error");
-
+  } on UnsupportedError {
+    print("on Unsupported Error: num2 의 값은 0입니다.");
     num2 = 1;
-    print("catch: num2를 1로 바꾸고 기존 코드를 다시 실행");
+    print('on Unsupported Error: num2 의 값을 1로 바꾸고, 재시도합니다.');
     result = num1 ~/ num2;
   }
 
