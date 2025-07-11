ca0df6431b94917926a2a3ec706342402e09b2ac
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 11:40:13 +0900

    Static 설명

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,44 +1,13 @@
-class Box {
-  late int item;
-
-  Box(int givenItem) : item = givenItem;
-
-  int get() {
-    return item;
-  }
-
-  void set(int newItem) {
-    item = newItem;
-  }
-}
-
-class StringBox {
-  late String item;
-
-  StringBox(String givenItem) : item = givenItem;
-
-  String get() {
-    return item;
-  }
-
-  void set(String newItem) {
-    item = newItem;
-  }
-}
-
-// 타입이 바뀌었다고 새롭게 클래스를 하나 더 만들면
-// 똑같은 기능을 하는 클래스가 하나 더 필요하게 되네?
-// 그러면 너무 코드 작성하는것도 귀찮고, 한 번에 합칠수는 없을까?
-// -> 이 때 Generic 을 사용하면 됨.
-
-// Generic을 적용한 클래스 선언 방법
-// class 클래스명<T> {}
 class NewBox<T> {
   late dynamic item;
+  // set 을 이용해서 값이 몇 번 바뀌었는지 저장할 용도로 만든 static 변수
+  // 객체가 필요 없고, NewBox 라는 클래스 전체가 공유하는 변수
+  static int changeCount = 0;
 
-  // 사용자가 입력한 givenValue 의 자료형을 항상 따라갈거야.
-  // 사용자가 1 을 입력하게 되면 T -> int 자료형이 되는거고
-  // 사용자가 "시계" 를 입력하게 되면 T -> String 자료형이 되는거고
+  // 우리가 이 클래스를 사용하기로 한 객체 에서만 사용할 수 있는 변수
+  // 객체 = 우리가 main 함수에서 var newNum1 = NewBox(); 이 코드에서 newNum1 을 뜻함.
+  int totalCount = 0;
+
   NewBox(T givenValue) {
     item = givenValue;
   }
@@ -49,15 +18,21 @@ class NewBox<T> {
 
   void set(dynamic newItem) {
     item = newItem;
+    changeCount = changeCount + 1;
+    totalCount = totalCount + 1;
   }
 }
 
 void main() {
   var newBox = NewBox("참돔");
+  var newBox2 = NewBox("참치");
 
   print(newBox.get());
-  newBox.set(34142314231423);
-  print(newBox.get());
-  newBox.set(1342.234);
-  print(newBox.get());
+
+  newBox.set(0);
+  newBox2.set(1);
+
+  print("newBox 객체가 변경된 횟수: ${newBox.totalCount}");
+  print("newBox2 객체가 변경된 횟수: ${newBox2.totalCount}");
+  print("NewBox 클래스 전체가 변경된 횟수: ${NewBox.changeCount}");
 }
