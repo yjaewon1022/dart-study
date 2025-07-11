fe7a8aec59820d6fc83313c51fad0ae581238c85
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 16:51:19 +0900

    우리가 직접 만든 클래스를 이용한 예외처리 학습

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,15 +1,20 @@
+// NegativeDivisorException 이라는 클래스를 만들건데,
+// Exception 이라는 가상클래스를 참조해서 구현할거야.
+class NegativeDivisorException implements Exception {
+  @override
+  String toString() {
+    return "0으로 숫자를 나눌 수 없습니다.";
+  }
+}
+
 int calc(int a, int b) {
   int result = 0;
 
-  try {
+  if (result == 0) {
+    // throw = 스스로 에러를 만들어서 코드 진행을 막고 싶을 때 사용하는 문법
+    throw NegativeDivisorException();
+  } else {
     result = a ~/ b;
-  } on UnsupportedError {
-    print("on Unsupported Error: num2 의 값은 0입니다.");
-    b = 1;
-    print('on Unsupported Error: num2 의 값을 1로 바꾸고, 재시도합니다.');
-    result = a ~/ b;
-  } catch (error) {
-    print("알수 없는 에러가 발생했습니다.");
   }
 
   return result;
@@ -22,6 +27,11 @@ void main() {
 
   try {
     result = calc(num1, num2);
+  } on NegativeDivisorException {
+    print("현재 num2가 0입니다.");
+    num2 = 1;
+    print("num2 를 1로 바꾸고 다시 시도합니다.");
+    result = num1 ~/ num2;
   } finally {
     print("결과는 : $result 입니다.");
   }
