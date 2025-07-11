56236b6ffff8f77124cef9bfa9ad78f2bae176df
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 14:29:22 +0900

    비동기 공부 p240 ~ 242

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -8,8 +8,33 @@
 
 // 4. await는 결과가 나올 때까지 기다렸다가, 다음 줄 코드를 실행합니다.
 
+doBG(int time, var jobEnd) {
+  if (time > 0) {
+    print("$time 초 남았습니다.");
+    Future.delayed(Duration(seconds: 1), () => doBG(time - 1, jobEnd));
+  } else {
+    print("밥이 준비되었습니다.");
+    jobEnd.activated = true;
+  }
+}
+
+class Flag {
+  late bool _flag;
+
+  Flag(bool givenFlag) : _flag = givenFlag;
+  bool get activated => _flag;
+  set activated(bool givenFlag) => _flag = givenFlag;
+}
+
 void main() async {
+  var finished = Flag(false);
+
   print("밥 주문하기");
-  await Future.delayed(Duration(seconds: 3), () => print("밥 도착!"));
+  await doBG(5, finished);
+
+  while (finished.activated == false) {
+    await Future.delayed(Duration(seconds: 1));
+  }
+
   print("밥 먹기");
 }
