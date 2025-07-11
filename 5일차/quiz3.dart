fe62784f0ea516a3ca138af4c990929d42e2c9ee
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 17:10:38 +0900

    Update quiz3.dart

--- 5일차/quiz3.dart
+++ 5일차/quiz3.dart
@@ -1,5 +1,5 @@
 // NoStringException 이라는 에러 클래스를 생성한 후,
-// main 함수에 num 이라는 변수에 10 을 넣어주세요.
-// 이후 num의 타입이 string 이 아닐 경우,
+// main 함수에 num 이라는 변수를 생성하고, 10 을 넣어주세요.
+// 이후 try-catch 문을 통해 num의 타입이 string 이 아닐 경우,
 // NoStringException 클래스를 통해 다음의 내용이 출력되게 구현하세요.
 // [해당 값은 문자열이 아닙니다.]
