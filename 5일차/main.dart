3d397a0403f53611e4f0ff17a4ef3ae19522e71f
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 16:38:53 +0900

    함수 내에서 에러를 처리하는 방법 학습

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,23 +1,27 @@
+int calc(int a, int b) {
+  int result = 0;
+
+  try {
+    result = a ~/ b;
+  } on UnsupportedError {
+    print("on Unsupported Error: num2 의 값은 0입니다.");
+    b = 1;
+    print('on Unsupported Error: num2 의 값을 1로 바꾸고, 재시도합니다.');
+    result = a ~/ b;
+  } catch (error) {
+    print("알수 없는 에러가 발생했습니다.");
+  }
+
+  return result;
+}
+
 void main() {
   int num1 = 1;
   int num2 = 0;
   int result = 0;
 
-  // try = 일단 먼저 실행을 해봐라
-  // catch = try 중 에러가 발생하면, try 실행을 멈추고 catch 이후의 내용을 실행해라.
-
-  // try-catch 문법 사용 방법: try {} catch(error) {}
-  // try-on 문법 사용 방법: try {} on 에러이름 {}
-  // finally = try {} catch(error){} 이 모든걸 수행하고 최종적으로 한 번 실행됨.
   try {
-    result = num1 ~/ num2; // 1 ~/ 0
-  } on UnsupportedError {
-    print("on Unsupported Error: num2 의 값은 0입니다.");
-    num2 = 1;
-    print('on Unsupported Error: num2 의 값을 1로 바꾸고, 재시도합니다.');
-    result = num1 ~/ num2;
-  } catch (error) {
-    print("알수 없는 에러가 발생했습니다.");
+    result = calc(num1, num2);
   } finally {
     print("결과는 : $result 입니다.");
   }
