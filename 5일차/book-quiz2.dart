cd813b687617958ab10d1ebdc2858d5f4adbd844
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-11 15:44:13 +0900

    async await 퀴즈 풀이

--- 5일차/book-quiz2.dart
+++ 5일차/book-quiz2.dart
@@ -1,12 +1,27 @@
 // Future.Delayed를 이용하여 다음과 같은 출력을 반환하는 프로그램을 만들어보세요
 // Delayed 함수에서는 [10초 뒤에 실행된 코드입니다.] 가 출력되어야 합니다.
-// 출력 결과 : 
+// 출력 결과 :
 // 안녕하세요
 // 10초 뒤에 실행된 코드입니다.
 // 안녕하세요
+// void main() async {
+//   print("안녕하세요");
+//   await Future.delayed(Duration(seconds: 10), () => print("10초 뒤에 실행된 코드입니다."));
+//   print("안녕하세요");
+// }
 
 // 비동기 함수인 sayHello 를 생성한 후,
 // 해당 함수가 실행된지 5초 후에 [Hello] 라는 문자열을 출력하는 코드를 작성해보세요.
-// 해당 함수가 종료된 후, main 함수에서는 [Hello 가 출력되었습니다.] 라는 
+// 해당 함수가 종료된 후, main 함수에서는 [Hello 가 출력되었습니다.] 라는
 // 문자열이 출력되어야 합니다.
 
+Future<String> sayHello() async {
+  return await Future.delayed(Duration(seconds: 5), () => "hello");
+}
+
+void main() async {
+  var test = await sayHello();
+
+  print(test);
+  print("Hello 가 출력되었습니다.");
+}
