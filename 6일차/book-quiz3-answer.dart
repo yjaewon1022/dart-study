793515eaa315b9848d39a0b5943d34b88d5e7205
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-14 15:21:34 +0900

    퀴즈3: map 을 이용한 입출력 관리 정답

--- 6일차/book-quiz3-answer.dart
+++ 6일차/book-quiz3-answer.dart
@@ -11,9 +11,12 @@
 // 식단표 출력 프로그램 - 급식을 진행하지 않는 날입니다.
 // 식단표 출력 프로그램 - 이어서 출력할까요? : 네
 // 식단표 출력 프로그램 - 날짜를 입력하세요 : 7/17
-// 식단표 출력 프로그램 -급식이 존재하지 않는 날입니다.
+// 식단표 출력 프로그램 - 급식이 존재하지 않는 날입니다.
 // 식단표 출력 프로그램 - 이어서 출력할까요? : 아니오
 // 프로그램을 종료합니다.
+
+import 'dart:convert';
+import 'dart:io';
 
 void main() {
   var dayInput, doAgainInput;
@@ -24,4 +27,34 @@ void main() {
     "7/15": "된장국, 잡곡밥, 어묵볶음, 비엔나소세지, 깍두기",
     "7/16": "휴무일",
   };
+
+  // while 에서 bool 을 조건으로 입력하는 경우,
+  // while(doAgain) 과 while(doAgain == true) 는 똑같은 동작을 한다.
+  while (doAgain == true) {
+    stdout.write("식단표 출력 프로그램 - 날짜를 입력하세요 :");
+    dayInput = stdin.readLineSync(); // 날짜입력
+
+    // 날짜 예외처리1. 입력한 날짜에 값이 존재하는가?
+    if (foods.containsKey(dayInput) == true) {
+      // 날짜 예외처리2. 입력한 날짜의 식단표 내용이 "휴무일"인가?
+      if (foods[dayInput] == "휴무일") {
+        stdout.writeln("식단표 출력 프로그램 - 급식이 진행하지 않는 날입니다.");
+      } else {
+        stdout.writeln("식단표 출력 프로그램 - $dayInput의 식단표 : ${foods[dayInput]}");
+      }
+    } else {
+      stdout.writeln("식단표 출력 프로그램 - 급식이 존재하지 않는 날입니다.");
+    }
+
+    stdout.write("식단표 출력 프로그램 - 이어서 출력할까요? :");
+    doAgainInput = stdin.readLineSync(encoding: utf8); // "네" 또는 "아니오" 를 입력
+
+    if (doAgainInput == "아니오") {
+      doAgain = false;
+    } else {
+      doAgain = true;
+    }
+  }
+
+  stdout.writeln("프로그램을 종료합니다.");
 }
