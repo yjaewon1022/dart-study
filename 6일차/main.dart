cd6e192e0e4cb1cdb4c1576e4140d1239c3525f5
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-14 16:13:27 +0900

    파일 읽기 예제 진행

--- 6일차/main.dart
+++ 6일차/main.dart
@@ -1,20 +1,51 @@
 import 'dart:io';
+import 'dart:convert';
 
-void main() {
-  var iList = <int>[];
-  var sList = <String>[];
+Future<String> readFileToString(String filename) async {
+  var file = File(filename);
+  // file.readAsString() -> 한 번에 파일을 통으로 가져와서 읽는다.
+  // 용량이 작거나 내용이 적은 파일을 조회할 때 용이하게 사용이 가능하다.
+  // 단, 용량이 크거나 내용이 많은 경우 파일을 읽을 때 오랜 시간이 걸린다.
+  String fileContent = await file.readAsString();
+  return fileContent;
+}
 
-  stdout.write("숫자 두 개를 , 를 통해 입력해주세요. (예시: 3, 3)");
-  var input = stdin.readLineSync();
-  sList = input!.split(',');
+Future<List<String>> readFileToList(String filename) async {
+  // Stream -> 하나의 파일을 부분 부분 잘라서 필요한 부분을 이용한다.
+  // 물을 틀 때 수도꼭지를 이용해서 틀었다가, 필요한 부분이 충족되면 잠시 수도꼭지를 잠구듯이
+  // 필요한 부분 부분을 나누어서 파일을 읽는다.
 
-  for (var item in sList) {
-    iList.add(int.parse(item));
+  // File(filename) -> filename의 파일을 찾는다.
+  // openRead() -> 파일을 Stream 형태로 읽어서 반환하겠다.
+  // transform(utf8.decoder) -> 가져온 파일을 utf8 유니코드로 변경하겠다.
+  // transform(LineSplitter()) -> 가져온 파일을 한 줄 한 줄 잘라 사용할 수 있게 변경하겠다.
+  Stream<String> lines = File(
+    filename,
+  ).openRead().transform(utf8.decoder).transform(LineSplitter());
+
+  try {
+    List<String> sList = []; // lines 에서 잘라냈던 코드 줄들을 저장할 변수
+
+    // 잘라낸 코드 줄들을 각각 분할해서 접근할 수 있게 해주는 반복문
+    await for (var line in lines) {
+      sList.add(line); // sList 리스트에 잘라낸 코드 줄들을 추가
+    }
+    return sList;
+  } catch (error) {
+    throw error;
   }
+}
 
-  // 화면에 값을 출력할 때, String 과 String 을 + 하게 되면 해당 문자열들끼리 붙여 쓴 값이 출력됨
-  // int 와 int 를 + 했을 땐, 두 수를 더해진 값이 출력됨
-  stdout.writeln(
-    "더하기 프로그램을 진행합니다. ${iList[0]} + ${iList[1]} = ${iList[0] + iList[1]}",
-  );
+void main() async {
+  try {
+    List<String> fileContent = await readFileToList(
+      "C:/Users/user/Desktop/testapp/dart-study/6일차/src.txt",
+    );
+
+    for (var fileLine in fileContent) {
+      print(fileLine);
+    }
+  } catch (error) {
+    print(error);
+  }
 }
