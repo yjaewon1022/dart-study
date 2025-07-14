21c07b69e5095aba3bb063a103e61b22bde7aae6
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-14 11:39:46 +0900

    사용자 입출력 시 기본값 지정 방법 강의

--- 6일차/main.dart
+++ 6일차/main.dart
@@ -3,24 +3,25 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  // stdout : standard output 단어를 줄여서 작성한 것
-  //          Dart에서 화면 출력 기능을 담당하고 있음
-  // stdin : standard input 단어를 줄여서 작성한 것
-  //         Dart에서 키보드 입력을 지원하고 있음
-  stdout.write("입력해주세요:");
-  var userInput = stdin.readLineSync(encoding: utf8);
-  stdout.writeln("당신은 이것을 입력했습니다.: $userInput");
+  var iList = <int>[]; // int형만 들어갈 수 있는 리스트구나!
+  stdout.write("첫 번째 숫자를 입력해주세요.:"); // 화면에 글씨 출력
+  var input = stdin.readLineSync(); // input 에 사용자가 입력한 값을 넣었구나!
+  iList.add(int.parse(input!));
+  // input 뒤에 ! 를 붙이게 되면 -> 컴퓨터에게 이 input은 무조건 값이 있을거야. 라고 명시하는 것
+  // iList.add(int.parse(input));
+  // 너가 input에 사용자가 값을 넣을 거라고 작성은 했지만,
+  // 사용자가 값을 아무것도 입력하지 않았을 때에 대한 대처가 필요해!
+  // int.parse(값) : 값을 int 형태로 변경할거야!
+  // iList에 int 형으로 바꾼 input을 담을거야!
 
-  while (userInput != "exit") {
-    stdout.write("입력해주세요:");
-    userInput = stdin.readLineSync(encoding: utf8);
-    stdout.writeln("당신은 이것을 입력했습니다.: $userInput");
-  }
-
-  stdout.writeln("입력이 종료되었습니다.");
-
-  // // writeln : 소괄호 안에 있는 내용을 출력하고, 들여쓰기를 진행한다.
-  // // write : 소괄호 안에 있는 내용을 출력한다.
-  // stdout.writeln("출력2");
-  // stdout.write("출력1");
+  stdout.write("두 번째 숫자를 입력해주세요.:");
+  input = stdin.readLineSync();
+  iList.add(int.parse((input?.isEmpty ?? true) ? '1' : input!));
+  // input ?? '1' 이 코드는
+  // input에 값이 있으면 그대로 input을 쓰고.
+  // input에 값이 없으면 '1' 을 써라.
+  // A ?? B -> A가 참(true)이면 A 를 쓰고, 거짓(false)이면 B를 써라.
+  stdout.writeln(
+    "더하기 프로그램을 진행합니다. ${iList[0]} + ${iList[1]} = ${iList[0] + iList[1]}",
+  );
 }
