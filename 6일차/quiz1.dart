f465e766f42ece4c4a3cd2259c0c82bf06a9d6b8
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-15 13:49:41 +0900

    퀴즈 1~3 주석처리

--- 6일차/quiz1.dart
+++ 6일차/quiz1.dart
@@ -1,19 +1,19 @@
-// 다음 코드에서 빠진 부분을 채워,
-// './6일차/quiz/temp.txt' 파일에 'Dart는 재미있어요!' 라는 문자열을 저장한 후,
-// 그 내용을 다시 읽어 출력하는 프로그램을 완성하세요.
+// // 다음 코드에서 빠진 부분을 채워,
+// // './6일차/quiz/temp.txt' 파일에 'Dart는 재미있어요!' 라는 문자열을 저장한 후,
+// // 그 내용을 다시 읽어 출력하는 프로그램을 완성하세요.
 
-import 'dart:io';
+// import 'dart:io';
 
-void main() async {
-  var file = File('./quiz/temp.txt');
+// void main() async {
+//   var file = File('./quiz/temp.txt');
 
-  // 파일에 문자열 저장 (빈칸을 채우세요)
-  var sink = _______________________;
-  sink.___________________;
-  await sink.close();
+//   // 파일에 문자열 저장 (빈칸을 채우세요)
+//   var sink = _______________________;
+//   sink.___________________;
+//   await sink.close();
 
-  // 저장한 내용을 다시 읽기 (빈칸을 채우세요)
-  String content = await _______________________;
+//   // 저장한 내용을 다시 읽기 (빈칸을 채우세요)
+//   String content = await _______________________;
 
-  print(content);
-}
+//   print(content);
+// }
