eaba23ba3ac6afbd51892f1a3aaaf9c375e10d62
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-15 17:00:28 +0900

    7일차 퀴즈 문제

---
+++ 7일차/quiz2.dart
@@ -0,0 +1,16 @@
+// localhost:8080 으로 접근할 수 있는 서버를 생성하고,
+// 크롬 검색창에서 localhost:8080/capital=Korea 과 같이 입력했을 때,
+// 다음과 같이 출력이 되는 기능을 만드세요.
+// [Korea의 수도는 Seoul 입니다.]
+// 단, 수도가 입력되지 않은 경우는 다음과 같이 출력되어야 합니다.
+// [유효하지 않은 수도를 입력하셨습니다.]
+
+Future main() async {
+  Map capital = {
+    "Korea": "Seoul",
+    "Japan": "Tokyo",
+    "China": "Beijing",
+    "Canada": "Ottawa",
+    "Austrailia": "Canberra",
+  };
+}
