5adf9ba4a4d47355620ce454a949550baf3d35c7
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-15 14:21:01 +0900

    http 통신 기본 학습

--- 7일차/main.dart
+++ 7일차/main.dart
@@ -1,66 +1,58 @@
+import 'dart:convert';
 import 'dart:io';
-import 'dart:async';
-import 'dart:math';
 
-Future<int> async1() async {
-  print("[4] async1(): 1 seond left.");
-  await Future.delayed(Duration(seconds: 1));
-  print("[4] async1(): finished.");
+Future main() async {
+  var ip = InternetAddress.loopbackIPv4;
+  var port = 4040;
 
-  return 10;
-}
+  var server = await HttpServer.bind(ip, port);
 
-Future<int> async2() async {
-  print("[4] async2(): 2 seond left.");
-  await Future.delayed(Duration(seconds: 2));
-  print("[4] async2(): 1 seond left.");
-  await Future.delayed(Duration(seconds: 1));
-  print("[4] async2(): finished.");
+  print("\$ server activated - ${server.address.address}:${server.port}");
 
-  return 20;
-}
+  // server 에 존재하는 request 들에 대해서 계속해서 반복하여 내부 코드를 실행하겠다.
+  await for (HttpRequest request in server) {
+    // try-catch 구조 : try {} catch(error) {}
+    // try 뒤에 있는 {} 의 내용을 실행하되, 해당 내용 실행 중 에러가 발생하면
+    // catch 이후에 있는 {} 의 내용으로 바꿔서 실행해라.
+    try {
+      // http://127.0.0.1:4040/ 에서 ip와 port를 제외한 이후 문자열이 / 만 있는지
+      // 확인하는 조건문
+      // request.uri.path -> 위의 url 에서 http://127.0.0.1:4040 을 제외한 그 다음 문자열
+      if (request.uri.path == '/') {
+        print('\$ http response is "Hello, World!".');
+        print('\$ send "200 OK".');
 
-Future<int> async3() async {
-  print("[4] async3(): 3 seond left.");
-  await Future.delayed(Duration(seconds: 3));
-  print("[4] async3(): 2 seond left.");
-  await Future.delayed(Duration(seconds: 2));
-  print("[4] async3(): 1 seond left.");
-  await Future.delayed(Duration(seconds: 1));
-  print("[4] async3(): finished.");
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..write("Hello, World!");
+      } else if (request.uri.path.contains('/add')) {
+        print('\$ http response is result of "add" operation.');
+        print('\$ send "200 OK".');
 
-  return 30;
-}
+        var varList = request.uri.path.split(',');
+        var result = int.parse(varList[1]) + int.parse(varList[2]);
 
-void main() {
-  // List<String> fruits = ["Apple is Red", '  Banana  ', 'Mango'];
-  // print("[1] Mango in List ? : ${fruits.contains('Mango')}");
-  // print("[1] Starts with 'Apple' ? : ${fruits[0].startsWith('Apple')}");
-  // print("[1] Ends with 'Red' : ${fruits[0].endsWith('Red')}");
-  // print("[1] Index of 'Red' : ${fruits[0].indexOf('Red')}");
-  // print("[1] Trim '  Banana  ' : ${fruits[1].trim()}");
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..write("${varList[1]} + ${varList[2]} = $result");
+      } else if (await File(request.uri.path.substring(1)).exists() == true) {
+        // request.uri.path -> port 번호까지 다 지운 후 남아있는 텍스트. 즉 /sample.txt
+        // .substring(1) -> 맨 앞에 있는 첫 번째 문자열을 제거하겠다. 즉 sample.txt
+        print('\$ http response is "${request.uri.path}" file transfer.');
+        print('\$ send "200 ok".');
 
-  // // Platform - dart:io 라이브러리에서 제공해주는 기능
-  // // Native 환경 -> 윈도우OS, 맥OS
-  // String os = Platform.operatingSystem;
-  // String path = Platform.script.toFilePath();
-  // print("[2] OS: $os");
-  // print("[2] Source File: $path");
+        var file = File(request.uri.path.substring(1));
+        var fileContent = await file.readAsString();
 
-  // // dart:math - 사칙연산 등 수학 계산을 지원해주는 라이브러리
-  // print("[3] max(2,4) : ${max(2, 4)}");
-  // print("[3] min(2,4) : ${min(2, 4)}");
-  // print("[3] e: $e");
-  // print("[3] pi: $pi");
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..headers.contentType = ContentType('text', 'plain', charset: "utf-8")
+          ..write(fileContent);
+      }
 
-  var t1 = DateTime.now();
-
-  Future.wait([async1(), async2(), async3()])
-      .then((List<int> nums) {
-        var t2 = DateTime.now();
-
-        var sum = nums.reduce((curr, next) => curr + next);
-        print('[4] Sum: $sum < Time: ${t2.difference(t1)} >');
-      })
-      .catchError(print);
+      await request.response.close();
+    } catch (error) {
+      print("\$ 요청 수행 중 에러 발생: $error");
+    }
+  }
 }
